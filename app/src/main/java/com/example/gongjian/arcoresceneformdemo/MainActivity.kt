b7c5788d8a8960fa1c7da958e3b59b13a33dd233
gongjian <chinagj@vip.qq.com>
2018-06-05 17:38:33 +0800

    加入依赖

--- app/src/main/java/com/example/gongjian/arcoresceneformdemo/MainActivity.kt
+++ app/src/main/java/com/example/gongjian/arcoresceneformdemo/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.gongjian.arcoresceneformdemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
+/**
+ * https://github.com/google-ar/arcore-android-sdk/releases  arcore 的apk下载地址
+ * */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
